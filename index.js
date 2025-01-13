1e65ffd8fc305cc82acdad697a9a6ec9c9956970
Erwin <ervin.sults@gmail.com>
2025-01-13 20:58:08 +0200

    write code for swagger and edit index

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express'); 
-const swaggerDocument = require('./docs/swagger.json'); 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
 
 const app = express();
 const port = 8080;
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/', (req, res) => {
-    res.send('Привет, мир!');
+const movies = [
+    { id: 1, title: 'Inception', genre: 'Sci-Fi', year: 2010 },
+    { id: 2, title: 'The Dark Knight', genre: 'Action', year: 2008 },
+    { id: 3, title: 'Forrest Gump', genre: 'Drama', year: 1994 }
+];
+
+app.get('/movies', (req, res) => {
+    res.json(movies);
 });
 
 app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
-    console.log(`Документация доступна на http://localhost:${port}/docs`);
+    console.log(`API работает на http://localhost:${port}`);
 });
