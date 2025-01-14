631e780b8b907a83fb12ae9582ca04ea158c1527
Erwin <ervin.sults@gmail.com>
2025-01-14 15:49:49 +0200

    change from json to yaml

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
-
-const express = require('express');
 const app = express();
 
 const movies = [
-    { id: 0, name: "The Matrix", price: 12.99 },
-    { id: 1, name: "Inception", price: 15.49 },
-    { id: 2, name: "Interstellar", price: 18.00 },
-    { id: 3, name: "The Dark Knight", price: 14.50 },
-    { id: 4, name: "Pulp Fiction", price: 10.00 },
-    { id: 5, name: "The Godfather", price: 16.99 },
-    { id: 6, name: "Forrest Gump", price: 11.50 },
-    { id: 7, name: "Gladiator", price: 13.00 }
+    { id: 1, name: "The Matrix", price: 12.99 },
+    { id: 2, name: "Inception", price: 15.49 },
+    { id: 3, name: "Interstellar", price: 18.00 },
+    { id: 4, name: "The Dark Knight", price: 14.50 },
+    { id: 5, name: "Pulp Fiction", price: 10.00 },
+    { id: 6, name: "The Godfather", price: 16.99 },
+    { id: 7, name: "Forrest Gump", price: 11.50 },
+    { id: 8, name: "Gladiator", price: 13.00 }
 ];
 
 app.get('/movies', (req, res) => {
@@ -21,14 +17,14 @@ app.get('/movies', (req, res) => {
 });
 
 app.get('/movies/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const movie = movies.find(m => m.id === id);
+    const id = parseInt(req.params.id, 10) - 1; 
+    const movie = movies[id];
 
-    if (movie) {
-        res.send(movie);
-    } else {
-        res.status(404).send({ error: "Movie not found" });
+    if (!movie) {
+        return res.status(404).send({ error: "Movie not found" }); 
     }
+
+    res.send(movie); 
 });
 
 const PORT = 8080;
