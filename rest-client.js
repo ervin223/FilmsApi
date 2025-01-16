c3164128ee033715d8d82b6cc2874e2f4f6cdbd5
Erwin <ervin.sults@gmail.com>
2025-01-16 13:04:17 +0200

    edit rest client for actors

--- rest-client.js
+++ rest-client.js
@@ -2,29 +2,27 @@ Vue.createApp({
     data() {
         return {
             films: [],
-            filmInModal: { id: null, name: null, price: null } 
         };
     },
     created() {
-        fetch('http://localhost:8080/movies')
-            .then(response => response.json())
-            .then(data => {
-                this.films = data;
-            })
-            .catch(error => console.error('Error fetching films:', error));
+        this.fetchMovies();
     },
     methods: {
-        async getFilm(id) {
-            try {
-                const response = await fetch(`http://localhost:8080/movies/${id}`);
-                const data = await response.json();
-                this.filmInModal = data;
-
-                const modal = new bootstrap.Modal(document.getElementById('filmInfoModal'));
-                modal.show();
-            } catch (error) {
-                console.error('Error fetching film details:', error);
-            }
+        fetchMovies() {
+            fetch('http://localhost:8080/movies')
+                .then(response => response.json())
+                .then(data => {
+                    this.films = data;
+                })
+                .catch(error => console.error('Error fetching movies:', error));
+        },
+        getFilmActors(id) {
+            fetch(`http://localhost:8080/movies/${id}/actors`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Actors:', data);
+                })
+                .catch(error => console.error('Error fetching actors:', error));
         }
     }
 }).mount('#app');
