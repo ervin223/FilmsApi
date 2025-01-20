3ba05cce6c8cf89016d8cc7771ca806a7a032332
Erwin <ervin.sults@gmail.com>
2025-01-20 10:18:30 +0200

    added CRUD -  edit, add new film, delete in the frontend

--- rest-client.js
+++ rest-client.js
@@ -3,7 +3,9 @@ Vue.createApp({
         return {
             films: [], 
             selectedFilm: {}, 
-            selectedFilmActors: [] 
+            selectedFilmActors: [], 
+            filmForm: { id: null, name: '', price: 0 }, 
+            isEditing: false 
         };
     },
     created() {
@@ -14,33 +16,80 @@ Vue.createApp({
             fetch('http://localhost:8080/movies')
                 .then(response => response.json())
                 .then(data => {
-                    this.films = data;
+                    this.films = data.map(film => ({
+                        id: film.id || null,
+                        name: film.name || 'Untitled',
+                        price: typeof film.price === 'number' ? film.price : 0
+                    }));
                 })
                 .catch(error => console.error('Error fetching movies:', error));
         },
-        async getFilmDetails(id) {
-            try {
-                const response = await fetch(`http://localhost:8080/movies/${id}`);
-                const data = await response.json();
-                this.selectedFilm = data;
-
-                const modal = new bootstrap.Modal(document.getElementById('filmDetailsModal'));
-                modal.show();
-            } catch (error) {
-                console.error('Error fetching film details:', error);
+        getOrCreateModal(modalId) {
+            const modalElement = document.getElementById(modalId);
+            if (!modalElement) {
+                console.error(`Modal with ID "${modalId}" not found.`);
+                return null;
             }
+            let modalInstance = bootstrap.Modal.getInstance(modalElement);
+            if (!modalInstance) {
+                modalInstance = new bootstrap.Modal(modalElement, { backdrop: true });
+            }
+            return modalInstance;
         },
-        async getFilmActors(id) {
-            try {
-                const response = await fetch(`http://localhost:8080/movies/${id}/actors`);
-                const data = await response.json();
-                this.selectedFilmActors = data;
+        showAddFilmModal() {
+            this.filmForm = { id: null, name: '', price: 0 };
+            this.isEditing = false;
+            const modal = this.getOrCreateModal('filmModal');
+            if (modal) modal.show();
+        },
+        showEditFilmModal(film) {
+            this.filmForm = { ...film };
+            this.isEditing = true;
+            const modal = this.getOrCreateModal('filmModal');
+            if (modal) modal.show();
+        },
+        saveFilm() {
+            const method = this.isEditing ? 'PUT' : 'POST';
+            const url = this.isEditing
+                ? `http://localhost:8080/movies/${this.filmForm.id}`
+                : 'http://localhost:8080/movies';
 
-                const modal = new bootstrap.Modal(document.getElementById('filmActorsModal'));
-                modal.show();
-            } catch (error) {
-                console.error('Error fetching film actors:', error);
-            }
+            fetch(url, {
+                method,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(this.filmForm)
+            })
+                .then(() => {
+                    this.fetchMovies();
+                    const modal = this.getOrCreateModal('filmModal');
+                    if (modal) modal.hide();
+                })
+                .catch(error => console.error('Error saving film:', error));
+        },
+        deleteFilm(id) {
+            fetch(`http://localhost:8080/movies/${id}`, { method: 'DELETE' })
+                .then(() => this.fetchMovies())
+                .catch(error => console.error('Error deleting film:', error));
+        },
+        getFilmActors(id) {
+            fetch(`http://localhost:8080/movies/${id}/actors`)
+                .then(response => response.json())
+                .then(data => {
+                    this.selectedFilmActors = data;
+                    const modal = this.getOrCreateModal('filmActorsModal');
+                    if (modal) modal.show();
+                })
+                .catch(error => console.error('Error fetching film actors:', error));
+        },
+        getFilmDetails(id) {
+            fetch(`http://localhost:8080/movies/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.selectedFilm = data;
+                    const modal = this.getOrCreateModal('filmDetailsModal');
+                    if (modal) modal.show();
+                })
+                .catch(error => console.error('Error fetching film details:', error));
         }
     }
 }).mount('#app');
