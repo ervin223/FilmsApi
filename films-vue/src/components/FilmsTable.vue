9c642fe2cc43c4ea897ec1abb47d9e280086bbec
Erwin <ervin.sults@gmail.com>
2025-02-06 14:28:49 +0200

    realise table(films) in frontend

--- films-vue/src/components/FilmsTable.vue
+++ films-vue/src/components/FilmsTable.vue
@@ -6,7 +6,29 @@
         Add New Film
       </button>
   
-      
+      <table class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th style="width: 25%">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="film in films" :key="film.id">
+            <td
+              @click="getFilmDetails(film.id)"
+              class="text-primary"
+              style="cursor: pointer;"
+            >
+              {{ film.name }}
+            </td>
+            <td>{{ film.price ? film.price.toFixed(2) : 'N/A' }} $</td>
+            <td>
+              <div class="btn-group">
+                <button
+                  class="btn btn-info btn-sm"
+                  @click="getFilmActors(film.id)"
                 >
                   Actors
                 </button>
@@ -34,7 +56,39 @@
         </tbody>
       </table>
   
-      
+      <div class="modal fade" id="filmModal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">
+                {{ isEditing ? 'Edit Film' : 'Add Film' }}
+              </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+              <div class="mb-3">
+                <label>Film Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="filmForm.name"
+                />
+              </div>
+              <div class="mb-3">
+                <label>Film Price</label>
+                <input
+                  type="number"
+                  class="form-control"
+                  v-model="filmForm.price"
+                />
+              </div>
+              <button
+                class="btn btn-success w-100"
+                @click="saveFilm"
+              >
+                Save
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -92,11 +146,47 @@
         </div>
       </div>
   
-      
+      <div class="modal fade" id="filmDetailsModal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Film Details</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+              <table class="table">
+                <tr>
+                  <th>ID</th>
+                  <td>{{ selectedFilm.id }}</td>
+                </tr>
+                <tr>
+                  <th>Name</th>
+                  <td>{{ selectedFilm.name }}</td>
+                </tr>
+                <tr>
+                  <th>Price</th>
+                  <td>{{ selectedFilm.price ? selectedFilm.price.toFixed(2) : 'N/A' }} $</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </template>
   
   <script>
-
+  import axios from 'axios'
+  import { Modal } from 'bootstrap'
+  
+  export default {
+    name: 'FilmsTable',
+    data() {
+      return {
+        films: [],
+        selectedFilm: {},
+        selectedFilmActors: [],
+        currentMovieId: null,
         newActorName: '',
         filmForm: { id: null, name: '', price: 0 },
         isEditing: false
